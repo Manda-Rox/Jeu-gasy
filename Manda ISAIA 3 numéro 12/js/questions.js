d254a919d18f9ff8b0340467ffc30219d5367e5a
Manda-Rox <randmanda3@gmail.com>
2023-04-16 19:44:33 +0200

    fin

---
+++ Manda ISAIA 3 numéro 12/js/questions.js
@@ -0,0 +1,70 @@
+function questions(numero)
+{
+	var chaine="";
+	switch(numero)
+	{
+		case 1:
+		chaine="En Quelle année la pandémie coronavirus arrive à Madagascar*2012*2020*2022*2023*2";
+		break;
+		case 2:
+		chaine=" Madagascar accède l'indépendance le*26Juin 1960*08Mars2000*21 décembre 2012*18 Avril 1960*1";
+		break;
+		case 3:
+		chaine="Qui était le président en 1975*Zafy Albert*Didier Ratsiaraka *Philbert Tsiranana*Manandafy*2";
+		break;
+		case 4:
+		chaine="Qui est élu en 2013*Andry Rajoelina*Hery Rajaonarimampianina*Ravalomanana*Edgard*2";
+		break;
+		case 5:
+		chaine="29 Mars 1947.*Insurréction*Assoption*Assassinat*Election*1";
+		break;
+		case 6:
+		chaine="Qui est shadow cabinet du Président Andry Rajoelina?.*Mamy RAVATOMANGA *Hajo Andrianairivelo *Emmanuel Macron*Vladmir poutine*1";
+		break;
+		case 7:
+		chaine="combien de province à Madagascar?.*2*4*6*8*3";
+		break;
+		case 8:
+		chaine="c'est la .... plus grande île du monde.*troisième*cinquième*deuxième*quatrième*4";
+		break;
+		case 9:
+		chaine="Dans quel océan l'île de Madagascar est-elle situé?.*l'océan Indien*l'océan Pacifique*L'océan Atlantique*L'Océan Antarqtique*1";
+		break;
+		case 10:
+		chaine="Quelle est la superficie totale de Madagascar?.*587 040km²*476 196km²*692 483km²*602 400km²*1";
+		break;
+		case 11:
+		chaine="Quelle est la population de Madagascar en 2010?.*21 281 844 habitants*28 566 732 habitants*18 421 974 habitants*18 000 000 habitants*1";
+		break;
+		case 12:
+		chaine="Autrefois,les Français ont colonisé Madagascar.Mais les Malagasy ont eu leur indépendance en:*1960*1948*1972*1996*1";
+		break;
+		case 13:
+		chaine="une grande partie de la faune et de la flore de Madagascar est unique au monde ,on dit que ce sont des espèces *Endémique*caduques*spécifique*Rares*1";
+		break;
+		case 14:
+		chaine="comment se nomme la vielle tradition des tribus malgaches consistant à envelopper des ossements dans de la soie?*Tromba*zanahary*Famadihana*Sova*3";
+		break;
+		case 15:
+		chaine="Que signifie la partie blanche du drapeau de Madagascar ?.*La pureté le riz la soie*La sagesse la neige le sable*1";
+		break;
+		case 16:
+		chaine="la partie rouge du drapeau?. *Le sang les minéraux les fruits*La terre, l'argile et le sang	*2";
+		break;
+		case 17:
+		chaine="Que représente la partie verte du drapeau?*l'arbre du voyageur*Le baobab*espoir*sagesse*1";
+		break;
+		case 18:
+		chaine="Qu'appelle_t_on le ravinala?.*Un arbre endémique*Une plante endémique*Un palmier endémique*Akondro*3";
+		break;
+		case 19:
+		chaine="Combien de Famille de lémuriens existe_t_il à Madagascar?.*Trois*Six*Huit*douze*3";
+		break;
+		case 20:
+		chaine="Madagascar est un pays...*francophone*Anglophone*Africophone*Asiatique*1";
+		break;
+		
+	}
+	
+	return chaine;
+}
